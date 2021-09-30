bb964a95765504fed99f3b702cba9d9eb550d49c
RUnexpert <aldali911@gmail.com>
2021-09-30 17:38:38 +0300

    feat: solve cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,17 +21,73 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default class VigenereCipheringMachine {
 
-  constructor (bool = true) {
+  constructor(bool = true){
     this.bool = bool;
   }
-
+ 
   encrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     if(message === undefined || key === undefined){
+         throw new Error('Incorrect arguments!');
+       }
+ 
+     const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+     let arr = [];
+     let messageUp = message.toUpperCase();
+     let keyUp = key.toUpperCase();
+    
+ 
+     for(let i = 0, j = 0; i < messageUp.length; i++) {
+       if(ABC.indexOf(messageUp[i]) === -1) {
+         arr.push(messageUp[i]);
+         continue;
+       } 
+ 
+       let keySumbol = ABC[((ABC.indexOf(messageUp[i]) + ABC.indexOf(keyUp[j])) % 26)];
+       j++;
+ 
+       if (j === keyUp.length) {
+          j = 0;
+       }
+       arr.push(keySumbol);
+     }
+ 
+       if (!this.bool) {
+         arr.reverse();
+       }
+       let result = arr.join('');
+       return result;
   }    
-
+ 
   decrypt(secret, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     if(secret === undefined || key === undefined){
+        throw new Error('Incorrect arguments!');
+     }
+ 
+     const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+     let arr = [];
+     let secretUp = secret.toUpperCase();
+     let keyUp = key.toUpperCase();
+ 
+     for(let i = 0, j = 0; i < secretUp.length; i++) {
+       if(ABC.indexOf(secretUp[i]) === -1) {
+         arr.push(secretUp[i]);
+         continue;
+       } 
+ 
+       let keySumbol = ABC[((ABC.indexOf(secretUp[i]) - ABC.indexOf(keyUp[j]) + 26) % 26)];
+       j++;
+ 
+       if (j === keyUp.length) {
+          j = 0;
+       }
+       arr.push(keySumbol);
+     }
+ 
+       if (!this.bool) {
+         arr.reverse();
+       }
+ 
+       let result = arr.join('');
+       return result;    
   }
 }
